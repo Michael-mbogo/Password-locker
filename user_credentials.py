ce395b1525aa840e1e572bcafffa82811b3008cc
Michael-mbogo <mikembogo17@gmail.com>
2018-06-08 13:11:58 +0300

    Add changes

--- user_credentials.py
+++ user_credentials.py
@@ -1,13 +1,12 @@
 class User:
     '''
-    Class that generates new instance of users
+    Class that generates new instance of users.
     '''
 
     user_list = []
 
-    def __init__(self,first_name,last_name,number,password)
+    def __init__(self,first_name,last_name,password):
 
         self.first_name = first_name
         self.last_name = last_name
-        self.number = number
-        self.email = email
+        self.password = password
