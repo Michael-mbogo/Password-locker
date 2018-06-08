270eca4d7ae3c1e3b4968bddeca1e2807cca74d9
Michael-mbogo <mikembogo17@gmail.com>
2018-06-08 12:37:01 +0300

    Add user details

---
+++ user_credentials.py
@@ -0,0 +1,13 @@
+class User:
+    '''
+    Class that generates new instance of users
+    '''
+
+    user_list = []
+
+    def __init__(self,first_name,last_name,number,password)
+
+        self.first_name = first_name
+        self.last_name = last_name
+        self.number = number
+        self.email = email
